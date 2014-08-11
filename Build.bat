b294ea96d098ead6d58d92940359aff1055c6297
rohitlodha <rohitlodha@gmail.com>
2014-08-12 02:47:26 +0530

    Added build.bat

---
+++ Build.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Release|Win32"
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Release|x64" 
