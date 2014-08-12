29ab3fd7a871085dffeb1b82f00a857a22c95e6d
rohitlodha <rohitlodha@gmail.com>
2014-08-12 15:37:51 +0530

    Updated strings

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Release|Win32"
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Release|x64" 
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Static Release|Win32"
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Static Release|x64" 
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Release|Win32"
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Release|x64" 
