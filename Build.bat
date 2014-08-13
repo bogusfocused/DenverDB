875367d1ac7fd190de43e41b7075da3e84b3d612
rohitlodha <rohitlodha@gmail.com>
2014-08-13 18:47:46 +0530

    updated build.bat to incluse setups

--- Build.bat
+++ Build.bat
@@ -2,3 +2,5 @@
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Static Release|x64" 
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Release|Win32"
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Release|x64" 
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Setup Release|Win32"
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" DenverDB.sln /Rebuild "Setup Release|x64" 
